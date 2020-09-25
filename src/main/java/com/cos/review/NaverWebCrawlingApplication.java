dc7d0be1c98e46eaca694fdef854d5a0e91a2ac6
김동민 <cokj1610@naver.com>
2020-09-25 23:06:41 +0900

    네이버 제품리뷰 크롤링하기

---
+++ src/main/java/com/cos/review/NaverWebCrawlingApplication.java
@@ -0,0 +1,13 @@
+package com.cos.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverWebCrawlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaverWebCrawlingApplication.class, args);
+	}
+
+}
